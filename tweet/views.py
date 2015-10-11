f3847c4ab3f084d99dde5d3929260eb387519909
bharath <bharathramh92@gmail.com>
2015-10-10 22:59:21 -0400

    added raw tweets for testing

--- tweet/views.py
+++ tweet/views.py
@@ -21,17 +21,18 @@ def open_url(search):
     top_level_url = twitter_search_base_url + "?q=" + urllib.parse.quote(search, safe='') + "&size=100"
     r = requests.get(top_level_url, auth= (twitter_username, twitter_password))
     data_json = r.json()
-    tweet_list = []
+    tweet_list, tweets_raw_list = [], []
     hashtags = defaultdict(int)
     for tweet in data_json['tweets']:
         tw = tweet['message']['body']
+        tweets_raw_list.append(tw)
         t = Tweet()
         for word in tw.split():
             if word[0] == "#":
                 hashtags[word.lower()] += 1
                 t.tweets[word.lower()] += 1
         tweet_list.append(tweet_list)
-    return dict(hashtags)
+    return dict(hashtags), tweets_raw_list
 
 
 
@@ -52,8 +53,8 @@ def index(request):
             # o = twitter_search_url + "?q=" + urllib.parse.quote(search, safe='') + "&size=10"
             # o = "https://b66707bb-5e6b-48fa-be61-116b4274081b:ndTn1LNeKx@cdeservice.mybluemix.net/api/v1/messages/search?q=kerala%20blasters&size=10"
             # print(o)
-            hashtags = open_url(search)
-            return render(request, 'tweet/search_result.html', {'hashtags': hashtags})
+            hashtags, tweets_raw_list = open_url(search)
+            return render(request, 'tweet/search_result.html', {'hashtags': hashtags, 'tweets_raw_list': tweets_raw_list})
 
     # if a GET (or any other method) we'll create a blank form
     else:
