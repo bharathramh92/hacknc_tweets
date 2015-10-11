0d0aeee85e8d21e20c950b625b53a080fd5f83f7
bharath <bharathramh92@gmail.com>
2015-10-10 22:46:02 -0400

    changed to tweet takes to 100

--- tweet/views.py
+++ tweet/views.py
@@ -9,24 +9,31 @@ import requests
 
 # Create your views here.
 
+class Tweet:
+    def __init__(self):
+        self.tweets = defaultdict(int)
+
+    def get_tweets(self):
+        return dict(self.tweets)
 
 def open_url(search):
 
-    top_level_url = twitter_search_base_url + "?q=" + urllib.parse.quote(search, safe='') + "&size=10"
+    top_level_url = twitter_search_base_url + "?q=" + urllib.parse.quote(search, safe='') + "&size=100"
     r = requests.get(top_level_url, auth= (twitter_username, twitter_password))
     data_json = r.json()
+    tweet_list = []
     hashtags = defaultdict(int)
     for tweet in data_json['tweets']:
         tw = tweet['message']['body']
-        # print(tw)
+        t = Tweet()
         for word in tw.split():
             if word[0] == "#":
                 hashtags[word.lower()] += 1
-    print(hashtags)
-    ht = {}
-    for k,v in hashtags.items():
-        ht[k] = v
-    return hashtags
+                t.tweets[word.lower()] += 1
+        tweet_list.append(tweet_list)
+    return dict(hashtags)
+
+
 
 
 
@@ -46,7 +53,7 @@ def index(request):
             # o = "https://b66707bb-5e6b-48fa-be61-116b4274081b:ndTn1LNeKx@cdeservice.mybluemix.net/api/v1/messages/search?q=kerala%20blasters&size=10"
             # print(o)
             hashtags = open_url(search)
-            return render(request, 'tweet/search_result.html', {'hashtags': dict(hashtags)})
+            return render(request, 'tweet/search_result.html', {'hashtags': hashtags})
 
     # if a GET (or any other method) we'll create a blank form
     else:
