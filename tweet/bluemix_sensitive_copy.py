74101c4334e760e9cfdf08d5c9eb20835b00568b
bharath <bharathramh92@gmail.com>
2016-02-27 12:32:53 -0500

    added bluemix sensitive copy

---
+++ tweet/bluemix_sensitive_copy.py
@@ -0,0 +1,7 @@
+# rename this file as bluemix_sensitive.py
+twitter_username = "bluemix twitter username"
+twitter_password = "bluemix twitter password"
+twitter_search_url = "https://" + twitter_username + ":" + twitter_password +\
+                     "@cdeservice.mybluemix.net/api/v1/messages/search"
+twitter_search_base_url = "https://cdeservice.mybluemix.net/api/v1/messages/search"
+twitter_count_base_url = "https://cdeservice.mybluemix.net/api/v1/messages/count"
